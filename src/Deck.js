5992a33786b20ec2828f619a6ad5f760f2b726a0
Mehmet Tenekeci <info@mehmettenekeci.com>
2019-05-25 22:49:16 +0300

    initial 5

--- src/Deck.js
+++ src/Deck.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
-import {View, Animated, PanResponder, Dimensions} from 'react-native';
+import {Animated, Dimensions, PanResponder, View} from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
+    static defaultProps = {
+        onSwipeRight: () => {
+        },
+        onSwipeLeft: () => {
+        }
+    };
+
     constructor(props) {
         super(props);
 
@@ -17,17 +25,34 @@ class Deck extends Component {
             },
             onPanResponderRelease: (event, gesture) => {
                 if (gesture.dx > SWIPE_THRESHOLD) {
-                    console.log('swipe right!');
+                    this.forceSwipe('right');
                 } else if (gesture.dx < -SWIPE_THRESHOLD) {
-                    console.log('swipe left!');
+                    this.forceSwipe('left');
                 } else {
                     this.resetPosition();
                 }
             }
         });
 
-        this.state = {panResponder, position};//this.panResponder = panResponder; this.position = position
+        this.state = {panResponder, position, index: 0};//this.panResponder = panResponder; this.position = position
     };
+
+    forceSwipe(direction) {
+        const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+        Animated.timing(this.state.position, {
+            toValue: {x, y: 0},
+            duration: SWIPE_OUT_DURATION
+        }).start(() => this.onSwipeComplete(direction));
+    }
+
+    onSwipeComplete(direction) {
+
+        const {onSwipeLeft, onSwipeRight, data} = this.props;
+        const item = data[this.state.index];
+        direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+        this.state.position.setValue({x: 0, y: 0});
+        this.setState({index: this.state.index + 1})
+    }
 
     resetPosition() {
         Animated.spring(this.state.position, {
