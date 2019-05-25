cbb1600f53e8b06e4610ffffb37ce246282fc9c1
Mehmet Tenekeci <info@mehmettenekeci.com>
2019-05-25 16:24:22 +0300

    initial 4

--- src/Deck.js
+++ src/Deck.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
-import {View, Animated, PanResponder} from 'react-native';
+import {View, Animated, PanResponder, Dimensions} from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 
 class Deck extends Component {
     constructor(props) {
@@ -12,18 +15,36 @@ class Deck extends Component {
             onPanResponderMove: (event, gesture) => {
                 position.setValue({x: gesture.dx, y: gesture.dy});
             },
-            onPanRespnderRelease: () => {
-
+            onPanResponderRelease: (event, gesture) => {
+                if (gesture.dx > SWIPE_THRESHOLD) {
+                    console.log('swipe right!');
+                } else if (gesture.dx < -SWIPE_THRESHOLD) {
+                    console.log('swipe left!');
+                } else {
+                    this.resetPosition();
+                }
             }
         });
 
         this.state = {panResponder, position};//this.panResponder = panResponder; this.position = position
     };
 
+    resetPosition() {
+        Animated.spring(this.state.position, {
+            toValue: {x: 0, y: 0}
+        }).start();
+    }
+
     getCardStyle() {
+
+        const {position} = this.state;
+        const rotate = position.x.interpolate({
+            inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+            outputRange: ['-120deg', '0deg', '120deg']
+        });
         return {
-            ...this.state.position.getLayout(),
-            transform: [{rotate: '45deg'}]
+            ...position.getLayout(),
+            transform: [{rotate}]
         }
     }
 
